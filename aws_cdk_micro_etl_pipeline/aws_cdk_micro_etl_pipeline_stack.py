16aa499387cbb4de2c8bb028f41204c4fde0168f
PHIDELIST <phidelisoluoch@gmail.com>
2024-02-11 16:23:21 +0300

    Create input and output s3 buckets

--- aws_cdk_micro_etl_pipeline/aws_cdk_micro_etl_pipeline_stack.py
+++ aws_cdk_micro_etl_pipeline/aws_cdk_micro_etl_pipeline_stack.py
@@ -2,6 +2,10 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_s3 as s3,
+    RemovalPolicy,
+    aws_s3_deployment as s3deploy,
 )
 from constructs import Construct
 
@@ -17,3 +21,14 @@ class AwsCdkMicroEtlPipelineStack(Stack):
         #     self, "AwsCdkMicroEtlPipelineQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        input_s3_bucket="micro-etl-input"
+        output_s3_bucket="micro-etl-output"
+          # S3 Bucket to host glue scripts
+        bucket_input = s3.Bucket(self, "RawDataS3bucket", bucket_name=input_s3_bucket,  versioned=False,
+                           removal_policy=RemovalPolicy.DESTROY,
+                           auto_delete_objects=True, block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        bucket_output = s3.Bucket(self, "ProcessedDataS3bucket", bucket_name=output_s3_bucket, versioned=False,
+                                  removal_policy=RemovalPolicy.DESTROY,
+                                  auto_delete_objects=True, block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+                                  )
+
