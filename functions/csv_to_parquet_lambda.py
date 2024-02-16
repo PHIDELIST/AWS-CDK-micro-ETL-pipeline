fec3589b0f649a0fe59868a69b5ca70cb17802af
PHIDELIST <phidelisoluoch@gmail.com>
2024-02-16 23:45:58 +0300

    adding support for json files, s3 upload script, athena query script

--- functions/csv_to_parquet_lambda.py
+++ functions/csv_to_parquet_lambda.py
@@ -1,20 +1,26 @@
 import awswrangler as wr
 import boto3
+import os
 
 def lambda_handler(event, context):
     # Get the S3 bucket names from the event
     input_bucket = event['Records'][0]['s3']['bucket']['name']
-    
     output_bucket = "micro-etl-output" 
-    
     input_s3_key = event['Records'][0]['s3']['object']['key']
+    file_extension = os.path.splitext(input_s3_key)[-1].lower()
+    output_s3_path = f"output-parquet-files/{os.path.basename(input_s3_key).replace(file_extension, '.parquet')}"
+
+
+
+    if file_extension == '.csv':
+        df = wr.s3.read_csv(f"s3://{input_bucket}/{input_s3_key}")
+    elif file_extension == '.json':
+        df = wr.s3.read_json(f"s3://{input_bucket}/{input_s3_key}")
+    else:
+        print(f"Unsupported file format: {file_extension}")
+        return
     
-    output_s3_path = f"output-parquet-files/{input_s3_key.split('/')[-1].replace('.csv', '.parquet')}"
-    
-    # Read CSV files from the input S3 bucket
-    df = wr.s3.read_csv(f"s3://{input_bucket}/{input_s3_key}")
-    
-    # Convert CSV to Parquet format and write to output bucket
+
     wr.s3.to_parquet(df, f"s3://{output_bucket}/{output_s3_path}")
     
     client = boto3.client('glue')
---
+++ scripts/upload_to_s3.py
@@ -0,0 +1,30 @@
+import boto3
+
+def upload_file_to_s3(file_path, bucket_name, object_name):
+    """
+    Uploads a file to an S3 bucket.
+
+    :param file_path: Path to the file to upload.
+    :param bucket_name: Name of the S3 bucket.
+    :param object_name: S3 object key.
+    :return: True if file was uploaded successfully, else False.
+    """
+
+    s3_client = boto3.client('s3')
+
+    try:
+        
+        s3_client.upload_file(file_path, bucket_name, object_name)
+    except Exception as e:
+        print(f"Failed to upload file '{file_path}' to S3 bucket '{bucket_name}' with object key '{object_name}': {e}")
+        return False
+    else:
+        print(f"File '{file_path}' uploaded successfully to S3 bucket '{bucket_name}' with object key '{object_name}'")
+        return True
+
+
+file_path = 'sample-data/data.json' 
+bucket_name = 'micro-etl-input'
+object_name = 'data1.json'
+
+upload_file_to_s3(file_path, bucket_name, object_name)
